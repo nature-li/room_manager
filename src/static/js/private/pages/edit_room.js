8ad379ef9d9e155e652b184c236ed57d53f17b66
liyanguo <lyg@meitu.com>
2018-06-27 21:40:13 +0800

    ok

--- src/static/js/private/pages/edit_room.js
+++ src/static/js/private/pages/edit_room.js
@@ -1,7 +1,29 @@
 var start_date = null;
 var end_date = null;
 
+$(document).ready(function () {
+    init_datetime_picker();
+});
+
 $(document).on('click', '.click_cell', function () {
     $(this).addClass("selected_cell");
     console.log($(this).find('span').html());
 });
+
+function init_datetime_picker() {
+    var when = new Date();
+
+    $('#electric_date').datepicker({
+        language: 'zh-CN',
+        autoclose: true,
+        todayHighlight: true
+    });
+    $("#start_date").datepicker('setDate', when);
+
+    $('#water_date').datepicker({
+        language: 'zh-CN',
+        autoclose: true,
+        todayHighlight: true
+    });
+    $("#end_date").datepicker('setDate', when);
+}
